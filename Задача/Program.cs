0c51368c79a91f2085b661995d2d401f4075a859
BogdanBAE <bogdan.elistratov@gmail.com>
2022-11-28 17:13:08 +0400

    Написал саму программу

---
+++ Задача/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write(" Введите элементы массива через пробел: ");
+String elements = Console.ReadLine();
+string[] array1 = elements.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
